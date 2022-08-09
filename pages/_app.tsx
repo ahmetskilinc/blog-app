bf32b0bfb5e9b075702fb267606b031261476c05
Ahmet Kilinc <akx9@icloud.com>
2022-08-10 00:17:17 +0100

    added progress bar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,13 @@ import type { AppProps } from "next/app";
 import Navbar from "../components/Navbar";
 import Head from "next/head";
 import Footer from "../components/Footer";
+import { Router } from "next/router";
+import NProgress from "nprogress"; //nprogress module
+import "nprogress/nprogress.css";
+
+Router.events.on("routeChangeStart", () => NProgress.start());
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
