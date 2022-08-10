ae96e2c81a1a8c55dda0ca1ad821ab62e6eb5299
Ahmet Kilinc <akx9@icloud.com>
2022-08-10 15:36:56 +0100

    add heading in category page

--- pages/c/[name].tsx
+++ pages/c/[name].tsx
@@ -7,6 +7,7 @@ import type { Post } from "../../types/Post";
 import type { Category } from "../../types/Category";
 import Categories from "../../components/Categories";
 import qs from "qs";
+import { useRouter } from "next/router";
 
 type Props = {
 	posts: Post[];
@@ -14,16 +15,20 @@ type Props = {
 };
 
 const Home: NextPage<Props> = ({ posts, categories }) => {
+	const router = useRouter();
 	return (
 		<Main>
-			<div className="grid lg:grid-cols-post grid-cols-1 gap-4">
-				<div>
-					<Posts posts={posts} />
+			<>
+				<h1 className="text-3xl font-bold">{`Posts in ${router.query.name}`}</h1>
+				<div className="grid lg:grid-cols-post grid-cols-1 gap-4">
+					<div>
+						<Posts posts={posts} />
+					</div>
+					<div className="flex mt-11 flex-col gap-6 relative lg:sticky top-0 self-start">
+						<Categories categories={categories} />
+					</div>
 				</div>
-				<div className="flex mt-11 flex-col gap-6 relative lg:sticky top-0 self-start">
-					<Categories categories={categories} />
-				</div>
-			</div>
+			</>
 		</Main>
 	);
 };
