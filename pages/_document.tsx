b5f0bebc37d657adaabfd2237a7d8964dec24253
Ahmet Kilinc <akx9@icloud.com>
2022-08-18 15:28:58 +0100

    fix

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,6 @@ export default function Document() {
 	return (
 		<Html lang="en">
 			<Head>
-				<title>{`Ahmet's Blog`}</title>
 				<meta
 					name="description"
 					content="My blog for tutorials and my experiences with web development."
