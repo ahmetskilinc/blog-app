c739044bed389d52fe09cb2a02e9453b8cfe9719
Ahmet Kilinc <akx9@icloud.com>
2022-08-18 15:28:00 +0100

    dix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -20,14 +20,6 @@ function MyApp({ Component, pageProps }: AppProps) {
 		<>
 			<Head>
 				<title>{`Ahmet's Blog`}</title>
-				<meta
-					name="description"
-					content="My blog for tutorials and my experiences with web development."
-				/>
-				<link
-					rel="stylesheet"
-					href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300;0,400;0,500;0,600;1,500;1,600&display=swap"
-				/>
 			</Head>
 			<Navbar />
 			<Main>
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,17 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
 	return (
 		<Html lang="en">
-			<Head />
+			<Head>
+				<title>{`Ahmet's Blog`}</title>
+				<meta
+					name="description"
+					content="My blog for tutorials and my experiences with web development."
+				/>
+				<link
+					rel="stylesheet"
+					href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300;0,400;0,500;0,600;1,500;1,600&display=swap"
+				/>
+			</Head>
 			<body className="bg-[#f1f1f1] text-[#1f1f1f] dark:bg-[#1f1f1f] dark:text-[#f1f1f1]">
 				<Main />
 				<NextScript />
