cc5e84e1dc0b1ffc7c592a7915dd7295944ce674
Ahmet Kilinc <akx9@icloud.com>
2022-08-20 01:48:31 +0100

    whoops

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,6 @@ import "../styles/prism.css";
 import type { AppProps } from "next/app";
 import Navbar from "../components/Navbar";
 import Head from "next/head";
-import Footer from "../components/Footer";
 import { Router, useRouter } from "next/router";
 import NProgress from "nprogress";
 import "nprogress/nprogress.css";
