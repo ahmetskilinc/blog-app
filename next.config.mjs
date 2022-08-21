352261f41b0d9bbffa3fe9cce64f4a797103323e
Ahmet Kilinc <akx9@icloud.com>
2022-08-22 00:32:40 +0100

    add XSS config to next.config

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,33 @@ import remarkFrontmatter from "remark-frontmatter";
 import remarkMdxFrontmatter from "remark-mdx-frontmatter";
 import remarkPrism from "remark-prism";
 
+const ContentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self';
+  child-src example.com;
+  style-src 'self' example.com;
+  font-src 'self';  
+`;
+
+const securityHeaders = [
+	{
+		key: "Content-Security-Policy",
+		value: ContentSecurityPolicy.replace(/\s{2,}/g, " ").trim(),
+	},
+	{
+		key: "X-Content-Type-Options",
+		value: "nosniff",
+	},
+	{
+		key: "X-Frame-Options",
+		value: "SAMEORIGIN",
+	},
+	{
+		key: "X-XSS-Protection",
+		value: "1; mode=block",
+	},
+];
+
 export default {
 	webpack: (config, options) => {
 		config.module.rules.push({
@@ -27,4 +54,13 @@ export default {
 	reactStrictMode: true,
 	swcMinify: true,
 	pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
+	async headers() {
+		return [
+			{
+				// Apply these headers to all routes in your application.
+				source: "/:path*",
+				headers: securityHeaders,
+			},
+		];
+	},
 };
