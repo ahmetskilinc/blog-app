7e9e98d33d3f6cb50fa904072767f3ad23e7aa41
Ahmet Kilinc <akx9@icloud.com>
2022-08-22 17:05:50 +0100

    added Favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -35,6 +35,10 @@ const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
 			<Head>
 				<title>{`Ahmet's Blog`}</title>
 				<link rel="canonical" href={`${config.appUrl}${router.pathname}`} />
+				<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+				<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+				<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+				<link rel="manifest" href="/site.webmanifest" />
 			</Head>
 			<Script
 				strategy="afterInteractive"
