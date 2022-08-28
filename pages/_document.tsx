3b6e8b5cefdd789a840ac196fc3f57420b035d88
Ahmet Kilinc <akx9@icloud.com>
2022-08-28 17:16:57 +0100

    font change

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -12,9 +12,11 @@ export default function Document() {
 					name="keywords"
 					content="Ahmet Kilinc, Blog, Ahmet Kilinc Dev Blog, Ahmet Kilinc Dev, Web Development, Front End, Frontend, Developer, AhmetK Dev Blog, AhmetK Dev"
 				/>
+				<link rel="preconnect" href="https://fonts.googleapis.com" />
+				<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
 				<link
+					href="https://fonts.googleapis.com/css2?family=Albert+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,400;1,500&display=swap"
 					rel="stylesheet"
-					href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300;0,400;0,500;0,600;1,500;1,600&display=swap"
 				/>
 			</Head>
 			<body className="bg-[#f1f1f1] text-[#1f1f1f] dark:bg-[#1f1f1f] dark:text-[#f1f1f1] overflow-x-hidden">
